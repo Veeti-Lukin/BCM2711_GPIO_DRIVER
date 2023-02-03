817fb5cd9e2d9bb81fc1e8d30ac7dcfb6581f5bb
Veeti Lukin <veeti.lukin@tuni.fi>
2023-02-03 16:17:18 +0200

    Add exception class to handle faulty use cases and other exceptions

--- bcm2711_gpio_driver.hh
+++ bcm2711_gpio_driver.hh
@@ -6,6 +6,9 @@
 #define BCM_2711_DRIVER_HH
 
 #include<cstdint>
+#include<string>
+#include<exception>
+#include<vector>
 
 // This means pin HIGH, true, 3.3 volts on a pin.
 // Can be given as argument to <set_output_pin()>
@@ -31,6 +34,37 @@ enum pin_function
     ALT_FUNC5 = 0b010
 };
 
+// Exception class. Instances of this class will be thrown in case of errors
+// or faulty use of the driver class
+class BCM2711_GPIO_DRIVER_EXCEPTION : public std::exception
+{
+public:
+    // Default constructor where  error message is given as parameter
+    BCM2711_GPIO_DRIVER_EXCEPTION(const std::string msg) :
+        message("Error: "+msg){}
+    // Constructor where error message can have placeholders that are marked with "*"
+    // values from *values_to_format* will be formatted in order to these spots
+    BCM2711_GPIO_DRIVER_EXCEPTION(const std::string msg,
+                                  std::vector<std::string> values_to_format) :
+        message("Error: " + msg), values_to_format(values_to_format)
+    {
+        // format the values to to placeholder spots
+        for (std::string& value : values_to_format)
+        {
+            message.replace(message.find("*"), 1, value);
+
+        }
+    }
+
+    virtual const char* what() const noexcept override
+        {
+            return message.c_str();
+        }
+
+private:
+        std::string message;
+        std::vector<std::string> values_to_format;
+};
 
 class BCM2711_GPIO_DRIVER
 {
