136841392ecf1c9dfaf19bb64615edd55dbd993e
Veeti Lukin <veeti.lukin@tuni.fi>
2023-02-03 17:49:13 +0200

    Add functionality for memorymapping register to virtual memory and storing pointer to the control registers

--- bcm2711_gpio_driver.cpp
+++ bcm2711_gpio_driver.cpp
@@ -1,4 +1,31 @@
+#include <system_error>
+#include <unistd.h>
+#include <fcntl.h>  // for open()
+#include <sys/mman.h> // for mmap()
+#include <errno.h>  // for errno errors
+#include <cstring> // to be used with strerror()
 #include "bcm2711_gpio_driver.hh"
+
+BCM2711_GPIO_DRIVER::BCM2711_GPIO_DRIVER()
+{
+
+    gpio_init();
+
+    // REGISTER OFFSETS ARE FOUND IN BCM2711_peripherals.pdf SECTION 5.2
+    // Pointer arithmetics works by multiplying the int to be added with
+    // the size of the pointer which the int is getting added to.
+    // So in this case +1 means 32bits or 4 bytes increacment.
+    GPFSEL0 = virtual_memory_gpio_base_ptr;
+    GPFSEL1 = virtual_memory_gpio_base_ptr + 1; //4 byte jump between registers
+    GPFSEL2 = virtual_memory_gpio_base_ptr + 2;
+    GPFSEL3 = virtual_memory_gpio_base_ptr + 3;
+    GPFSEL4 = virtual_memory_gpio_base_ptr + 4;
+    GPFSEL5 = virtual_memory_gpio_base_ptr + 5;
+    GPSET0 = virtual_memory_gpio_base_ptr + 7;  // 8 byte jump between register "sections"
+    GPSET1 = virtual_memory_gpio_base_ptr + 8;
+    GPCLR0 = virtual_memory_gpio_base_ptr + 10;
+    GPCLR1 = virtual_memory_gpio_base_ptr + 11;
+}
 
 void BCM2711_GPIO_DRIVER::setup_gpio_function(uint8_t pin_num, pin_function func)
 {
@@ -8,4 +35,42 @@ void BCM2711_GPIO_DRIVER::setup_gpio_function(uint8_t pin_num, pin_function func
 bool BCM2711_GPIO_DRIVER::set_output_pin(uint8_t pin_num, bool value)
 {
 
+}
+
+void BCM2711_GPIO_DRIVER::gpio_init()
+{
+    // check if progrm is run as root/sudo
+    // (linux-kernel-call geteuid:
+    //          https://man7.org/linux/man-pages/man2/getuid.2.html)
+    if (geteuid() != 0)
+    {
+        throw BCM2711_GPIO_DRIVER_EXCEPTION("Driver needs to be run with ROOT privilages");
+    }
+
+    // Open the master /dev/mem devicefile for obtaining the handle to physical memory
+    // will set errno variable in case of error
+    // (linux-kernel-call open:
+    //          https://man7.org/linux/man-pages/man2/open.2.html)
+    auto memfd = open("/dev/mem", (O_RDWR | O_SYNC));
+
+    // test if opening /dev/mem failed
+    if (memfd < 0)
+    {
+        // strerror() turns the errno integer into a human readable error message.
+        throw BCM2711_GPIO_DRIVER_EXCEPTION(strerror(errno));
+    }
+
+    // Memory map the physical address range where control registers are located
+    // to virtual address space.
+    // Will set errno variable in case of error.
+    // first argument being NULL the kernel chooses where in the virtual address space to map
+    // https://man7.org/linux/man-pages/man2/mmap.2.html
+    virtual_memory_gpio_base_ptr = (uint32_t*)mmap(NULL, 4096, (PROT_READ | PROT_WRITE),
+                                    MAP_SHARED, memfd, GPIO_BASE_ADDRESS);
+
+    // check if memorymapping failed
+    if (virtual_memory_gpio_base_ptr == MAP_FAILED)
+    {
+       throw BCM2711_GPIO_DRIVER_EXCEPTION(strerror(errno));
+    }
 }
--- bcm2711_gpio_driver.hh
+++ bcm2711_gpio_driver.hh
@@ -62,8 +62,8 @@ public:
         }
 
 private:
-        std::string message;
-        std::vector<std::string> values_to_format;
+    std::string message;
+    std::vector<std::string> values_to_format;
 };
 
 class BCM2711_GPIO_DRIVER
@@ -82,7 +82,42 @@ public:
     bool set_output_pin(uint8_t pin_num, bool value);
 
 private:
+    // bcm2711-peripherals.pdf section 5.2 states:
+    // "The GPIO register base address is 0x7e20_0000".
+    // However addresses starting at 0x7e00_0000 are in VPU address space.
+    // If bcm2711 is running in "low peripheral mode" (default on rpi4)
+    // this address range is re-mapped to start at 0xFE00_0000
+    // in arm physical address space.
+    const uint32_t GPIO_BASE_ADDRESS = 0xFE200000;
 
+    // There are 58 General-Purpose Input/Output (GPIO) pin lines in BCM2711
+    // However rpi4 has only 40 pins, of which 28 are BCM2711 GPIO pins
+    const uint8_t GPIO_PIN_AMOUNT = 58;
+
+    // This pointer points to start (more spesifically to 32 first bits)
+    // of address range where GPIO controlling registers are memory mapped
+    // in the virtual address space.
+    volatile uint32_t* virtual_memory_gpio_base_ptr;
+
+
+    // CONTROL REGISTER POINTERS
+    // Each register is 32 bits long
+    volatile uint32_t* GPFSEL0;
+    volatile uint32_t* GPFSEL1;
+    volatile uint32_t* GPFSEL2;
+    volatile uint32_t* GPFSEL3;
+    volatile uint32_t* GPFSEL4;
+    volatile uint32_t* GPFSEL5;
+    volatile uint32_t* GPSET0;
+    volatile uint32_t* GPSET1;
+    volatile uint32_t* GPCLR0;
+    volatile uint32_t* GPCLR1;
+
+    // Handles the memorymapping the physical address range
+    // where control registers are located to virtual address space.
+    // this method will be called by the classes constructor
+    // TODO: Rename??
+    void gpio_init();
 };
 
 
